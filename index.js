6621dc1239b1df831122ba5dec2a853b2ae589e4
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2021-11-20 14:48:30 -0600

    photo author displayed

--- index.js
+++ index.js
@@ -18,4 +18,8 @@
 
  fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=varanasi")
  .then(res => res.json())
- .then(data => console.log(data))
+    .then(data => {
+        document.body.style.backgroundImage = `url(${data.urls.full})`
+        document.getElementById("author").textContent = `By: ${data.user.name}`
+    })
+    
